5c966c7b4ae45e5fe77152935e5bdaef9dc857e9
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2023-10-24 09:58:58 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,1 +1,5 @@
+function taro68(){alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
 
+function taro68(){document.getElementByld('who68').innerHTML='太郎';}
+
+function hanako68(){document.getElementByld('who68').innerHTML='花子';}
