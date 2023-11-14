4610d80f9ac437fcb8650635d2f56dcfb82a6b32
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2023-11-14 09:23:48 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,26 +1,12 @@
-function　hyoji(){{
-  //表示する文字
-var str="一文字ずつ表示します。";}
-  //テキストボックスの文字の数
-var cnt=document.timer.moji.value.lenght;}
-  //文字が全部表示されているか確認
+function　hyoji(){var str="一文字ずつ表示します。";var cnt=document.timer.moji.value.lenght;
 
-
-if(cnt<11){
-  //現在より1文字多く切り出して表示
-   document.timer.moji.value=str.substr(0,cnt+1);}
-else{
-  //全て表示されたら、空文字に戻す
-   document.timer.moji.value="";}
-function startfnc(){
-  //関数hyoji()を1000ミリ秒間隔で呼び出す
-   setInterval("hyoji()",1000);}
+if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
+else{document.timer.moji.value="";}
+function startfnc(){setInterval("hyoji()",1000);}
 
 
 const str='Mozilla';
-
 console.log(str.substr(1,2));
   //expected output:"oz"
-
 console.log(str.subst(2));
   //expected output:"zilla"
