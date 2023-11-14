898f475503a2387397345b7cd4cb76e6e510d1f2
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2023-11-14 10:16:00 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,6 +1,7 @@
-function mes(){alert(３秒経ちました！");}
+・・・
+function mes(){ alert("３秒経ちました！"); }
 
-function hyoji13(){var str13="一文字ずつ表示します。";
+function hyoji13(){ var str13 = "一文字ずつ表示します。";
 
 var cnt13 = document.timer13.moji13.value.length;
 
@@ -8,5 +9,5 @@ if( cnt13 < 11 ){ document.timer13.moji13.value = str13.substr(0,cnt13+1);}
 
 else{ document.timer13.moji13.value = ""; }
 
-setTimeout("hyoji13()",1000;)}
+setTimeout("hyoji13()",1000);}
 
