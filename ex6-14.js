ed0969f4fee6dcf6eba0a9d355521c7cf006cce2
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2023-12-23 16:39:57 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange3(){ document.getElementById('hangyo614').src='./image/hangyo2.jpeg'; }
-function imgchange4(){ document.getElementById('hangyo614').src='./image/hangyo1.jpeg'; }
+function imgchange3(){document.getElementById('hangyo614').src='./image/hangyo2.jpeg';}
+function imgchange4(){document.getElementById('hangyo614').src='./image/hangyo1.jpeg';}
