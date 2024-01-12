8728e6f232e5c8b32fbfbc52958d396919e6653b
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-13 01:16:14 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,3 +1,4 @@
-//function add(){var x = document.getElementById('input1').value; var y = document.getElementById('input2').value; alert(Number(x)+Number(y));}//
-
-function add(){var x = document.getElementById('input1').value; var y = document.getElementById('input2').value; alert(Number(x)+Number(y));}
+function add72(){
+  var x = document.getElementById('input1').value; 
+  var y = document.getElementById('input2').value;
+  alert(Number(x)+Number(y));}
