8eab91cf2d033a3a35183a103b1b88906e8d068d
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-13 00:39:02 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,7 +1,22 @@
-function nazo76(){
-  if(document.getElementById('answer76').value != '夢'){
+function uru711(){
+  var year = document.getElementById
+  if(document.getElementById('answer711').value != '夢'){
     alert('あたり！');
   }else{
     alert('はずれ！');
+  }
+}
+
+function add710(){
+  var x = document.getElementById('input5').value;
+  var y = document.getElementById('input6').value;
+  var bmi = Number(x)/(Number(y)**2);
+
+  if(bmi < 18.5){
+    alert('低体重');
+  }else if(bmi < 25){
+    alert('普通');
+  }else{
+    alert('肥満');
   }
 }
