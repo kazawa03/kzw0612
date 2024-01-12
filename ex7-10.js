38a08f43509223a16b71bdf694e5e4b751b07c59
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-12 23:49:00 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -2,6 +2,12 @@ function add710(){
   var x = document.getElementById('input5').value;
   var y = document.getElementById('input6').value;
   var bmi = Number(x)/(Number(y)**2);
-  
-  
-  alert(bmi);}
+
+  if(bmi < 18.5){
+    alert('低体重');
+  }else if(bmi < 25){
+    alert('普通');
+  }else{
+    alert('肥満');
+  }
+}
