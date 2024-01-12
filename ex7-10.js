4ae2606fc849fdc8a19035b255bf1d0129eeb4af
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-12 23:29:39 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,1 +1,7 @@
-function add(){var x = document.getElementById('input3').value; var y = document.getElementById('input4').value; alert(Number(x)/(Number(y)**2));}
+function add710(){
+  var x = document.getElementById('input5').value;
+  var y = document.getElementById('input6').value;
+  var bmi = Number(x)/(Number(y)**2);
+  
+  
+  alert(bmi);}
