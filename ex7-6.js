1c1a2a3ec0651144584503cc821e7a212c37f32a
kazawa03 <131092055+kazawa03@users.noreply.github.com>
2024-01-12 22:37:41 +0900

    Update ex7-6.js

--- ex7-6.js
+++ ex7-6.js
@@ -1,1 +1,1 @@
-function abekobe(){if(document.getElementById('answer').value != '夢'){alert('あたり！');}else{alert('はずれ！');}}
+function nazo76(){if(document.getElementById('answer76').value != '夢'){alert('あたり！');}else{alert('はずれ！');}}
